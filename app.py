5e914a686e777ea5dd6c4af0c6c8648b44f62ed3
manasjaiswal <mjswl12@gmail.com>
2022-11-13 17:29:38 +0530

    structure ci/cd

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return 'Manas Jaiswal'
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+
+  
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup,find_packages
+from typing import List
+
+#Declaring variables for setup functions
+PROJECT_NAME="MLops(BigSales)"
+VERSION="0.0.1"
+AUTHOR="Manas Jaiswal"
+DESCRIPTION="Practicing Mlops Project"
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT="-e ."
+
+def get_requirements_list() -> List[str]:
+    """
+    This function returns the list of requiremnets for the project in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list=[name.replace("\n","") for name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description= DESCRIPTION,   
+packages=find_packages(),
+install_requires=get_requirements_list()
+)            
